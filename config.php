478f5d6c5630f3e504741d4670fd5e321013d78e
Alice <88071202+alicecamargo@users.noreply.github.com>
2023-03-31 14:56:05 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'root';
+    $dbName = 'formulario-alice';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+ ?>
