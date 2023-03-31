95d57ee858d2ec36e25401e12d1ab64a3306fbec
Alice <88071202+alicecamargo@users.noreply.github.com>
2023-03-31 15:44:37 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = 'root';
-    $dbName = 'formulario-alice';
-
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
- ?>
